84246b0d518ad8b78f4831cc15377f93d8879e77
Sabrina <sanutaayvi@gmail.com>
2021-02-28 14:02:04 +0600

    event  listener

--- JS/main.js
+++ JS/main.js
@@ -3,3 +3,8 @@ const author = document.querySelector('#author');
 const year = document.querySelector('#year');
 const btn = document.querySelector('.btn');
 const bookList = document.querySelector('#book-list');
+
+btn.addEventListener('click', function (e) {
+    e.preventDefault();
+
+});
